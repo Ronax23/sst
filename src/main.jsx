c83453a26f822193a409eef6f4b04d4fe0671d44
ronax23 <bhavya.raheja1998@gmail.com>
2026-01-22 20:55:54 +0530

    added lazy laoding

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
 import { StrictMode } from 'react'
+import React, { Suspense, lazy } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import LandingPage from './Components/LandingPage.jsx';
-import ContactUs from './Components/ContactUs.jsx';
-import MainPage from './Components/MainPage.jsx';
-import OurTeam from './Components/OurTeam.jsx';
-import Terms from './Components/Terms.jsx';
-import Workshop from './Components/Workshop.jsx';
+const LandingPage=lazy(()=> import ('./Components/LandingPage.jsx'));
+const ContactUs= lazy(()=> import ('./Components/ContactUs.jsx'));
+const MainPage= lazy(()=>import ('./Components/MainPage.jsx'));
+const OurTeam= lazy(()=>import ('./Components/OurTeam.jsx'));
+const Terms= lazy(()=>import ('./Components/Terms.jsx'));
+const Workshop= lazy(()=>import ('./Components/Workshop.jsx'));
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <Suspense fallback={<div className="d-flex justify-content-center mt-5">Loading...</div>}>
       <Routes>
         <Route path="/" element={<MainPage />}>
           <Route path="/" element={<LandingPage />} />
@@ -24,7 +26,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="workshop" element={<Workshop />} />
         </Route>
       </Routes>
-          
+          </Suspense>
     </BrowserRouter>
   </StrictMode>,
 )
